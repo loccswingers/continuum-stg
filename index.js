07ea805b7beb841a7f91403b3d043cab44268dc8
loccswingers <144750641+loccswingers@users.noreply.github.com>
2023-11-03 12:33:58 -0500

    I1

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ function Search(){
   if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
   else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
   // bring the user to the page
-  window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+ // window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  window.location.replace(__uv$config.prefix + __uv$config.encodeUrl(url));
 }
 
 function go(url){
   window.navigator.serviceWorker.register('./sw.js', {scope: __uv$config.prefix})
-  window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  window.location.replace(__uv$config.prefix + __uv$config.encodeUrl(url));
 }
 
 function isUrl(val = ''){
