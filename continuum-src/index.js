2ec0c7741c6abc28d7d3a5aee91f7f9bae32eb7a
loccswingers <144750641+loccswingers@users.noreply.github.com>
2023-11-11 23:18:04 -0600

    Update index.js

--- continuum-src/index.js
+++ continuum-src/index.js
@@ -1,18 +1,29 @@
-function Search(){
-  let inputT = document.getElementById("urlbar");
+function RegisterWorker(){
   window.navigator.serviceWorker.register('./sw.js', {scope: __uv$config.prefix})
-  // does it have a link or a search?
-  let url = inputT.value.trim();
-  if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-  else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-  // bring the user to the page
- // window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-  window.location.replace(__uv$config.prefix + __uv$config.encodeUrl(url));
 }
 
 function go(url){
-  window.navigator.serviceWorker.register('./sw.js', {scope: __uv$config.prefix})
+  RegisterWorker()
   window.location.replace(__uv$config.prefix + __uv$config.encodeUrl(url));
+}
+
+function Search(){
+  let inputT = document.getElementById("urlbar");
+  let url = inputT.value.trim();
+  if(!isUrl(url)){
+  //  url = 'https://www.google.com/search?q=' + url;
+    go('https://www.google.com/search?q=' + url);
+    else if (url.startsWith('https://') || url.startsWith('http://')){
+      go(url);
+    }
+  }
+/*  if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+  else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+  RegisterWorker()
+  // bring the user to the page
+ // window.location.replace(__uv$config.prefix + __uv$config.encodeUrl(url));
+  let finalURL = __uv$config.prefix + __uv$config.encodeUrl(url)
+  window.location.replace("go.html?title=Chrome&url=" + finalURL) */
 }
 
 function isUrl(val = ''){
